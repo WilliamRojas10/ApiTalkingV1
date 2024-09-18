32a1c4432014c58e44c108dcd4213faab870b12c
William Rojas <william.rojas@itscordoba.edu.ar>
2024-09-18 18:59:26 -0300

    add of class library dao_library

--- ApiTalking/Data/MyDbContext.cs
+++ ApiTalking/Data/MyDbContext.cs
@@ -16,26 +16,25 @@ namespace ApiTalking.Data
         public DbSet<Comment> Comments { get; set; }
         public DbSet<Reaction> Reactions { get; set; }
 
-        //Para recorrer el enum UserStatus
-        protected override void OnModelCreating(ModelBuilder modelBuilder)
-        {
-            modelBuilder.Entity<User>().Property(u => u.UserStatus)
-                .HasConversion(
-                    v => v.ToString(),
-                    v => (UserStatus)Enum.Parse(typeof(UserStatus), v));
-            modelBuilder.Entity<Post>().Property(p => p.PostStatus)
-                .HasConversion(
-                    v => v.ToString(),
-                    v => (PostStatus)Enum.Parse(typeof(PostStatus), v));
-            modelBuilder.Entity<Comment>().Property(c => c.CommentStatus)
-                .HasConversion(
-                    v => v.ToString(),
-                    v => (CommentStatus)Enum.Parse(typeof(CommentStatus), v));
-            modelBuilder.Entity<Reaction>().Property(r => r.ReactionStatus)
-                .HasConversion(
-                    v => v.ToString(),
-                    v => (ReactionStatus)Enum.Parse(typeof(ReactionStatus), v));
-
-        }
+        // //Para recorrer el enum UserStatus
+         protected override void OnModelCreating(ModelBuilder modelBuilder)
+         {
+             modelBuilder.Entity<User>().Property(u => u.UserStatus)
+                 .HasConversion(
+                     v => v.ToString(),
+                     v => (UserStatus)Enum.Parse(typeof(UserStatus), v));
+             modelBuilder.Entity<Post>().Property(p => p.PostStatus)
+                 .HasConversion(
+                     v => v.ToString(),
+                     v => (PostStatus)Enum.Parse(typeof(PostStatus), v));
+             modelBuilder.Entity<Comment>().Property(c => c.CommentStatus)
+                 .HasConversion(
+                     v => v.ToString(),
+                     v => (CommentStatus)Enum.Parse(typeof(CommentStatus), v));
+             modelBuilder.Entity<Reaction>().Property(r => r.ReactionStatus)
+                 .HasConversion(
+                     v => v.ToString(),
+                     v => (ReactionStatus)Enum.Parse(typeof(ReactionStatus), v));
+         }
     }
 }
